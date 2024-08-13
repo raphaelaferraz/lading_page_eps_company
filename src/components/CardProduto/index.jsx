adadb8be3500cf8eb810500775fac801c4dfedef
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-13 13:11:58 -0300

    feat: adiciona componente de card produto

---
+++ src/components/CardProduto/index.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+import BotaoPrimario from '../BotaoPrimario';
+
+export default function CardProduto({ titulo, descricao, textoBotao, imagem }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles["container__container-imagem"]}>
+        <img src={imagem} alt={titulo} className={styles["container__container-imagem__imagem"]} />
+      </div>
+
+      <div className={styles["container__container-texto"]}>
+        <p className={`${styles["container__container-texto__titulo"]}`}>{titulo}</p>
+        <p className={styles["container__container-texto__descricao"]}>{descricao}</p>
+        <div className={styles["container__container-texto__botao"]}>
+          <BotaoPrimario 
+            texto={textoBotao}
+            caminho='/'
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+CardProduto.propTypes = {
+  titulo: PropTypes.string.isRequired,
+  descricao: PropTypes.string.isRequired,
+  textoBotao: PropTypes.string.isRequired,
+  imagem: PropTypes.string.isRequired,
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Titulo from "../../components/Titulo";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import CardAplicacoes from "../../components/CardAplicacoes";
+import CardProduto from "../../components/CardProduto";
 
 export default function Home() {
   return (
@@ -19,6 +20,13 @@ export default function Home() {
         icone="/home-card-aplicacoes.svg"
         titulo="Contrapiso Leve e Resistente"
         descricao="O isopor é uma solução leve e resistente para contrapisos, proporcionando uma base estável e duradoura para diversos tipos de revestimento. Ideal para quem busca praticidade e qualidade na construção e reforma de pisos."
+      />
+
+      <CardProduto 
+        titulo="Placas de Isopor"
+        descricao="Placas de isopor para isolamento térmico e acústico, com diversas espessuras e tamanhos. Ideal para quem busca conforto térmico e acústico em ambientes residenciais e comerciais."
+        textoBotao="Faça seu orçamento"
+        imagem="/placas-card-produto.png"
       />
       <Footer />
     </main>
